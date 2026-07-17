733c1cdef527e2940d2b24ba36008c7d03a337ac
monalisad1900-SRE <monalisad1900@gmail.com>
2026-07-16 21:36:11 -0400

    extra space added

--- shared/utils.py
+++ shared/utils.py
@@ -13,10 +13,3 @@ def get_logger(name: str) -> logging.Logger:
         level=logging.INFO,
     )
     return logging.getLogger(name)
-
-
-def get_aws_session(region: str = "us-east-1"):
-    """Return a boto3 session.
-    Reads credentials from environment / IAM role."""
-    import boto3
-    return boto3.Session(region_name=region)
